270f85d9efa9023c68a63841b7300da15c97d5c1
Dixit Regmi <dixitregmiofficial@gmail.com>
2024-01-20 22:43:36 +0545

    intersectionObserver study

--- main.js
+++ main.js
@@ -1,3 +1,32 @@
+//scroll event animation
+//for box
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add('box')
+        }else{
+            entry.target.classList.remove('box')
+        }
+    })
+})
+//for text
+const observer2 = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add('texts')
+        }else{
+            entry.target.classList.remove('texts')
+        }
+    })
+})
+
+let hiddenBox = document.querySelectorAll('.hiddenBox');
+let hiddenDisc = document.querySelectorAll('.hiddenTexts');
+
+hiddenBox.forEach((el)=> observer.observe(el));
+hiddenDisc.forEach((el)=> observer2.observe(el));
+
+// for click event
 let capsules = document.querySelectorAll(".capsule");
 
 capsules.forEach((uniqueCapsule) => {
@@ -11,3 +40,4 @@ capsules.forEach((uniqueCapsule) => {
         uniqueCapsule.classList.add('clicked');
     });
 });
+
